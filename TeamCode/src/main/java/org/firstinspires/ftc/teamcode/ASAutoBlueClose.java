c96968eb41497278802ee8514dd5a460b8d5ed12
Oshawhat <85427862+Oshawhaat@users.noreply.github.com>
2026-03-02 11:14:55 -0600

    add an auto that should use the correct directions

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ASAutoBlueClose.java
@@ -0,0 +1,111 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+
+@Autonomous(name = "Pedro Blue Close - fixed?", /*preselectTeleOp="Your Drive Code Here",*/ group="pedroblue")
+//@Disabled
+public class ASAutoBlueClose extends ASAuto {
+    private final Pose startPose = new Pose(72-52, 72+42, Math.toRadians(90));
+    private final Pose launchPose = new Pose(72-14, 72+10, Math.toRadians(-135));
+
+    public AS_Action[] getActions() {
+
+        AS_Action[] launchSequence = {
+                // Get ready for launching
+                new ASOpenStopper(this),
+                new ASSpinLauncher(this),
+
+                // Move to shooting position
+                new ASFollowPath(this, follower.pathBuilder()
+                        .addPath(new BezierLine(follower::getPose, launchPose))
+                        .setLinearHeadingInterpolation(follower.getHeading(), launchPose.getHeading())
+                        .build()
+                ),
+
+                // Launch!
+                new ASWaitForLauncher(this),
+                new ASWait(this, 500),
+
+                new ASSpinPusher(this),
+                new ASSpinIntake(this),
+                new ASWait(this, 800),
+                new ASSpinIntake(this, -.3),
+                new ASWait(this, 350),
+                new ASSpinIntake(this),
+                new ASSpinPusher(this, 1.5),
+
+                new ASWait(this, 1500),
+
+                // Reset
+                new ASStopLauncher(this),
+                new ASStopIntake(this),
+                new ASStopPusher(this)
+        };
+
+        AS_Action[] actions = {
+                // ======================= AUTO START ======================= //
+
+                // Init
+                new ASSetStartingPose(this, startPose),
+
+                // Launch!
+                new ASActionSequence(this, launchSequence),
+
+                // Get ready to intake
+                new ASCloseStopper(this),
+                new ASSpinIntake(this),
+                new ASSpinPusher(this),
+
+                // Grab line and hit gate
+                new ASFollowPath(this, follower.pathBuilder()
+                        // Grab line
+                        .addPath(new BezierCurve(
+                                launchPose,
+                                new Pose(72-18, 72-12),
+                                new Pose(72-62, 72-12)
+                        ))
+                        .setTangentHeadingInterpolation()
+                        // Hit gate
+                        .addPath(new BezierCurve(
+                                follower::getPose,
+                                new Pose(72-28, 72-17),
+                                new Pose(72-53, 72-2)
+                        ))
+                        .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(170))
+                        .setTValueConstraint(0.95)
+                        // Get out smoothly
+                        .addPath(new BezierLine(
+                                follower::getPose,
+                                new Pose(72-28, 72-7)
+                        ))
+                        .setLinearHeadingInterpolation(Math.toRadians(180), Math.toRadians(-135))
+                        .setNoDeceleration()
+                        .build()
+                ),
+
+                new ASStopPusher(this),
+                new ASStopIntake(this),
+
+                // Launch!
+                new ASActionSequence(this, launchSequence),
+
+                // Cycling time
+                new ASFollowPath(this, follower.pathBuilder()
+                        .addPath(new BezierCurve(
+                                launchPose,
+                                new Pose(72-18, 72-12),
+                                new Pose(72-58, 72-12)
+                        ))
+                        .setLinearHeadingInterpolation(follower.getHeading(), Math.toRadians(140))
+                        .build()),
+
+                // ======================== AUTO END ======================== //
+        };
+
+        return actions;
+    }
+}
